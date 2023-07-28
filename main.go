e0d540258a5463793b7aeaf8cefd747ec0b9bedc
satriatama <satriatama121@gmail.com>
2023-07-28 10:01:04 +0700

    first commit

--- main.go
+++ main.go
@@ -1,18 +1,4 @@
 package main
-
-import (
-	pasiencontrollers "github.com/satriatama/klinik-backend-go/controllers/pasienControllers"
-
-	"github.com/gofiber/fiber/v2"
-	"github.com/satriatama/klinik-backend-go/models"
-)
-
 func main() {
-	models.ConnectDatabase()
-	app := fiber.New()
-
-
-	app.Get("/api/v1/pasien", pasiencontrollers.Index)
-
-	app.Listen(":8000")
+	println("Hello, World!")
 }
